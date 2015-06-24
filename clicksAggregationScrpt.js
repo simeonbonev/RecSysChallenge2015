6b9bcf63f0cbd00ed3c39664029fd5607dcfc349
simeonbonev <asimeonbonev@gmail.com>
2015-06-25 01:41:04 +0300

    Experimenting with aggregation

--- clicksAggregationScrpt.js
+++ clicksAggregationScrpt.js
@@ -1,5 +1,24 @@
-db.sampleclicks.aggregate(
-   [
-     { $group : { _id : "$sessionId", clicks: { $push: {timestamp: "$timestamp", itemId: "$itemId", category: "$category"} } } }
-   ]
-)
+function aggregateClicks() {
+    print('Aggregate clicks');
+    printjson(
+        db.sampleclicks.aggregate(
+           [
+             { 
+                $group : 
+                    { 
+                        _id : "$sessionId", 
+                        clicks: { 
+                            $push: {
+                                timestamp: "$timestamp", 
+                                itemId: "$itemId", 
+                                category: "$category"
+                            } 
+                        },
+                        clicksCount: {$sum: 1},
+                        distinctItems: {$addToSet: "$itemId"}  
+                    } 
+            }
+           ]
+        )
+    );
+}
